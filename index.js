f536543b3409dd190e632fa83d0622459f0af02d
mindmazing <67432331+Mindmazing@users.noreply.github.com>
2026-07-08 10:39:38 -0600

    feat: detect when player places in occupied tile

--- index.js
+++ index.js
@@ -22,19 +22,98 @@ const GameBoard = (() => {
     board[yCor - 1][xCor - 1] = symbol;
   };
 
-  return { getBoard, resetBoard, placeMark };
+  const selectTile = (xCor, yCor) => {
+    return board[yCor - 1][xCor - 1];
+  };
+
+  return { getBoard, resetBoard, placeMark, selectTile };
 })();
 
 // Player Factory Function
 function Player(name, symbol) {
-  let score = 0;
+  let winner = false;
   const getSymbol = () => symbol;
   const getScore = () => score;
   const increaseScore = () => {
     score++;
   };
-  const resetScore = () => {
-    score = 0;
+  const resetPlayer = () => {
+    winner = false;
   };
-  return { name, getSymbol, getScore, increaseScore, resetScore };
+  const isWinner = () => winner;
+  const setWinner = () => {
+    winnner = true;
+  };
+
+  return {
+    name,
+    getSymbol,
+    getScore,
+    increaseScore,
+    resetPlayer,
+    isWinner,
+    setWinner,
+  };
 }
+
+// change when setting inputs from DOM
+player1 = Player("Fredy", "X");
+player2 = Player("Julia", "O");
+
+const GameLogic = (() => {
+  let currentPlayer = player1;
+  let rounds = 0;
+  let xCor, yCor;
+
+  const getCurrentPlayerCoors = () => {
+    alert(`Player: ${currentPlayer.name}, your turn`);
+    // change when setting inputs from DOM
+    xCor = +prompt("xcor");
+    yCor = +prompt("ycor");
+  };
+
+  const placeCurrentPlayerMark = () => {
+    GameBoard.placeMark(xCor, yCor, currentPlayer.getSymbol());
+    rounds++;
+  };
+
+  const changeTurn = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+    rounds++;
+  };
+
+  const boardIsFull = () => {
+    return rounds === 9;
+  };
+
+  const playerWon = () => {
+    currentPlayer.setWinner();
+  };
+
+  const isInvalidTile = () => {
+    let tileSelected = GameBoard.selectTile(xCor, yCor);
+    console.log(tileSelected);
+    return tileSelected !== "";
+  };
+
+  const isGameWon = () => {
+    let currentPlayerSymbol = currentPlayer.getSymbol();
+    currentPlayer.setWinner();
+  };
+
+  const restartGameLogic = () => {
+    rounds = 0;
+    xCor = null;
+    yCor = null;
+    currentPlayer = player1;
+  };
+
+  return {
+    placeCurrentPlayerMark,
+    changeTurn,
+    boardIsFull,
+    isInvalidTile,
+    getCurrentPlayerCoors,
+    restartGameLogic,
+  };
+})();
